32342545f577fa1d30774670b21a2897418a2c21
Andry Mironov <andry.mironov94@gmail.com>
2019-04-08 18:17:25 +0300

    Week 3. Mongoose Population

--- conFusionServer/routes/dishRouter.js
+++ conFusionServer/routes/dishRouter.js
@@ -12,6 +12,7 @@ dishRouter
 	.route('/')
 	.get((req, res, next) => {
 		Dishes.find({})
+			.populate('comments.author')
 			.then(
 				dishes => {
 					res.statusCode = 200
@@ -56,6 +57,7 @@ dishRouter
 	.route('/:dishId')
 	.get((req, res, next) => {
 		Dishes.findById(req.params.dishId)
+			.populate('comments.author')
 			.then(
 				dish => {
 					console.log('Dish created ', dish)
@@ -108,6 +110,7 @@ dishRouter
 	.route('/:dishId/comments')
 	.get((req, res, next) => {
 		Dishes.findById(req.params.dishId)
+			.populate('comments.author')
 			.then(
 				dish => {
 					if (dish != null) {
@@ -129,12 +132,17 @@ dishRouter
 			.then(
 				dish => {
 					if (dish != null) {
+						req.body.author = req.user._id
 						dish.comments.push(req.body)
 						dish.save().then(
 							dish => {
-								res.statusCode = 200
-								res.setHeader('Content-Type', 'application/json')
-								res.json(dish)
+								Dishes.findById(dish._id)
+									.populate('comments.author')
+									.then(dish => {
+										res.statusCode = 200
+										res.setHeader('Content-Type', 'application/json')
+										res.json(dish)
+									})
 							},
 							err => next(err)
 						)
@@ -185,6 +193,7 @@ dishRouter
 	.route('/:dishId/comments/:commentId')
 	.get((req, res, next) => {
 		Dishes.findById(req.params.dishId)
+			.populate('comments.author')
 			.then(
 				dish => {
 					if (dish != null && dish.comments.id(req.params.commentId) != null) {
@@ -227,9 +236,13 @@ dishRouter
 						}
 						dish.save().then(
 							dish => {
-								res.statusCode = 200
-								res.setHeader('Content-Type', 'application/json')
-								res.json(dish)
+								Dishes.findById(dish._id)
+									.populate('comments.author')
+									.then(dish => {
+										res.statusCode = 200
+										res.setHeader('Content-Type', 'application/json')
+										res.json(dish)
+									})
 							},
 							err => next(err)
 						)
@@ -255,9 +268,13 @@ dishRouter
 						dish.comments.id(req.params.commentId).remove()
 						dish.save().then(
 							dish => {
-								res.statusCode = 200
-								res.setHeader('Content-Type', 'application/json')
-								res.json(dish)
+								Dishes.findById(dish._id)
+									.populate('comments.author')
+									.then(dish => {
+										res.statusCode = 200
+										res.setHeader('Content-Type', 'application/json')
+										res.json(dish)
+									})
 							},
 							err => next(err)
 						)
