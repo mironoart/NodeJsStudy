689a163050fc1480c40dce9c432467dcc529e804
Andry Mironov <andry.mironov94@gmail.com>
2019-04-09 17:17:24 +0300

    Week 4.Passport Facebook

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -4,6 +4,7 @@ var User = require('./models/user')
 var JwtStrategy = require('passport-jwt').Strategy
 var ExtractJwt = require('passport-jwt').ExtractJwt
 var jwt = require('jsonwebtoken') // used to create, sign, and verify tokens
+var FacebookTokenStrategy = require('passport-facebook-token')
 
 var config = require('./config.js')
 
@@ -36,12 +37,39 @@ exports.jwtPassport = passport.use(
 
 exports.verifyAdmin = (req, res, next) => {
 	if (!req.user.admin) {
+		var err = new Error('You are not authorized to perform this operation!')
 		res.statusCode = 403
-		res.end('You are not authorized to perform this operation!')
-		err => next(err)
+		return next(err)
 	} else {
 		next()
 	}
 }
 
+exports.facebookPassport = passport.use(
+	new FacebookTokenStrategy(
+		{
+			clientID: config.facebook.clientId,
+			clientSecret: config.facebook.clientSecret
+		},
+		(accessToken, refreshToken, profile, done) => {
+			User.findOne({ facebookId: profile.id }, (err, user) => {
+				if (err) {
+					return done(err, false)
+				}
+				if (!err && user !== null) {
+					return done(null, user)
+				} else {
+					user = new User({ username: profile.displayName })
+					user.facebookId = profile.id
+					user.firstname = profile.name.givenName
+					user.lastname = profile.name.familyName
+					user.save((err, user) => {
+						if (err) return done(err, false)
+						else return done(null, user)
+					})
+				}
+			})
+		}
+	)
+)
 exports.verifyUser = passport.authenticate('jwt', { session: false })
--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	secretKey: '12345-67890-09876-54321',
-	mongoUrl: 'mongodb://localhost:27017/conFusion'
+	mongoUrl: 'mongodb://localhost:27017/conFusion',
+	facebook: {
+		clientId: '590412311461108',
+		clientSecret: '6fae2f489056e98089282a5e8e04ee25'
+	}
 }
